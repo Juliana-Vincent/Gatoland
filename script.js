749a8c8b6f92bf55ed7077466cbff3994ca5d1aa
Vincent <110050186+Juliana-Vincent@users.noreply.github.com>
2023-12-27 16:30:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("plusLinie").addEventListener("click", function() {
+  const linie = document.getElementById("linie");
+  const neueLinie = document.createElement("div");
+  neueLinie.className = "margo";
+  neueLinie.innerHTML = '<input type="text">';
+  linie.appendChild(neueLinie);
+});
+
+var checkboxes = document.querySelectorAll('.tik');
+var button = document.getElementById('submitButton');
+
+checkboxes.forEach(ch => {
+  ch.addEventListener('change', function() {
+    var allChecked = Array.from(checkboxes).every(ch => ch.checked);
+    button.disabled = !allChecked;
+  });
+});
+
+
