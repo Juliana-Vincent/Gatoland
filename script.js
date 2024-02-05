6617c9ea75c6a939f361ec26a6c683287db66bbc
Vincent <110050186+Juliana-Vincent@users.noreply.github.com>
2024-02-05 19:33:30 +0100

    commit

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ checkboxes.forEach(ch => {
 });
 
 
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'gato',
+  password: '4chan',
+  port: 5432,
+});
+
+app.post('/submit', async (req, res) => {
+  const { username, email, phonenumber, kitten, coupons } = req.body;
+
+  const result = await pool.query(
+    'INSERT INTO gato (username, email, phonenumber, kitten, coupons) VALUES ($1, $2, $3, $4, $5)',
+    [username, email, phonenumber, kitten, coupons]
+  );
+
+  res.send('Data submitted successfully');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
