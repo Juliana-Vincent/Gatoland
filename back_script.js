070a734321d437f25a698cb7bf98f704facaceb2
Vincent <110050186+Juliana-Vincent@users.noreply.github.com>
2024-02-06 13:05:55 +0100

    commit6

---
+++ back_script.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '4chan',
+  port: 5432,
+});
+
+app.post('/submit', async (req, res) => {
+  const { username, email, phonenumber, kitten, coupons } = req.body;
+
+  if (!username || !email || !phonenumber || !kitten || !coupons) {
+    return res.status(400).send('Missing data');
+  }
+  try {
+    const result = await pool.query(
+      'INSERT INTO gato (username, email, phonenumber, kitten, coupons) VALUES ($1, $2, $3, $4, $5)',
+      [username, email, phonenumber, kitten, coupons]
+    );
+    res.send('Data submitted successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("plusLinie").addEventListener("click", function() {
   const linie = document.getElementById("linie");
   const neueLinie = document.createElement("div");
   neueLinie.className = "margo";
-  neueLinie.innerHTML = '<input type="text" name="code">';
+  neueLinie.innerHTML = '<input type="text" name="code2">';
   linie.appendChild(neueLinie);
 });
 
@@ -14,40 +14,4 @@ checkboxes.forEach(ch => {
     var allChecked = Array.from(checkboxes).every(ch => ch.checked);
     button.disabled = !allChecked;
   });
-});
-
-
-const express = require('express');
-const { Pool } = require('pg');
-const app = express();
-app.use(express.json());
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '4chan',
-  port: 5432,
-});
-
-app.post('/submit', async (req, res) => {
-  const { username, email, phonenumber, kitten, coupons } = req.body;
-
-  if (!username || !email || !phonenumber || !kitten || !coupons) {
-    return res.status(400).send('Missing data');
-  }
-  try {
-    const result = await pool.query(
-      'INSERT INTO gato (username, email, phonenumber, kitten, coupons) VALUES ($1, $2, $3, $4, $5)',
-      [username, email, phonenumber, kitten, coupons]
-    );
-    res.send('Data submitted successfully');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
